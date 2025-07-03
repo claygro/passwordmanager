e3af63f614d1c2c04aa197f11535cb46ef26080a
claygro <dhamala80130@gmail.com>
2025-07-03 14:43:40 +0545

    adding url to connecting with backend with render

--- frontend/src/config/Api.config.jsx
+++ frontend/src/config/Api.config.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const ApiConnection = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://passwordmanager-backend-dg52.onrender.com",
 });
 export default ApiConnection;
